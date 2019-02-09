867e9d5e5cfaa046cd957e6f780caca58a57c42b
netham45 <netham45@gmail.com>
2019-02-08 19:19:31 -0700

    Update wan_down.php

--- wan_down.php
+++ wan_down.php
@@ -5,9 +5,10 @@ require_once("config.inc");
 require_once("notices.inc");
 require_once("openvpn.inc");
 require_once("interfaces.inc");
-
-interface_bring_down('wan');
+$config['interfaces']['wan']['disabled'] = 1;
+unset($config['interfaces']['wan']['enable']);
+write_config();
+interface_bring_Down('wan','true');
 $message = sprintf("Interface %s pinning down",$if);
 log_error($message);
-
 ?>
