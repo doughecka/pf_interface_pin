e365bba3b67186e8edc6a5f17dadc83f41d45343
netham45 <netham45@gmail.com>
2019-05-02 20:22:58 -0600

    Update wan_down.php
    Sets the route to use the CARP IP when it's not master for WAN access

--- wan_down.php
+++ wan_down.php
@@ -5,10 +5,11 @@ require_once("config.inc");
 require_once("notices.inc");
 require_once("openvpn.inc");
 require_once("interfaces.inc");
+exec("route add default 192.168.0.3");
 $config['interfaces']['wan']['disabled'] = 1;
 unset($config['interfaces']['wan']['enable']);
 write_config();
 interface_bring_Down('wan','true');
 $message = sprintf("Interface %s pinning down",$if);
 log_error($message);
-?>
+?>   
